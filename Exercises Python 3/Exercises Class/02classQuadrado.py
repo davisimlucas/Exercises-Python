679b52b1b9229ea1110d54acd52072165f2b59f1
davisimlucas <davisimoeslucas@gmail.com>
2023-06-18 20:16:19 -0300

    ex 2 class

---
+++ Exercises Python 3/Exercises Class/02classQuadrado.py
@@ -0,0 +1,22 @@
+# Crie uma classe que modele um quadrado:
+#   Atributos: Tamanho do lado
+#   Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área
+
+class quadrado:
+    def __init__(self, lado):
+        self.lado = lado
+
+    def ladoChange(self, change):
+        self.lado = change 
+    
+    def printLado(self):
+        return self.lado
+       
+    def area(self):
+        return self.lado ** 2
+
+quad = quadrado(5)
+print(quad.printLado())
+quad.ladoChange(9)
+print(quad.printLado())
+print(quad.area())
